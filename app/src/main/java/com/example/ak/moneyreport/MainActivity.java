e37316407e79c52a737beef787eada93bc22642e
Ankit Karnany <ankitkarnany@gmail.com>
2015-10-03 17:13:26 +0530

    Basic Layout Ready

---
+++ app/src/main/java/com/example/ak/moneyreport/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ak.moneyreport;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Data is send to report activity when clicked in the action bar
+        switch (item.getItemId()) {
+            case R.id.add:
+                break;
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
